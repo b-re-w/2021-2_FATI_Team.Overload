4bf4e352a2063e905659b423469063da1d438c54
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-08-31 15:05:18 +0900

    .

--- My_Projects/Jupyter/FATI_Main.py
+++ My_Projects/Jupyter/FATI_Main.py
@@ -147,9 +147,8 @@ class TeamOverload(object):
         driver = None
 
         def watchdog(driver):
-            def drive(mode, gap, reverse=False):
+            def drive(mode, gap, desired_angle, reverse=False):
                 duration = 10000
-                desired_angle = read_z()
                 go = self.zumi.forward if not reverse else self.zumi.reverse
                 while True:
                     if mode == 1:
@@ -170,7 +169,7 @@ class TeamOverload(object):
 
             dvm = [None, None]  # [drive_mode, obstacle_detected]
             while True:
-                print("watchdog: %d, %d" % (drive_mode[0], drive_mode[1]))
+                #print("watchdog: %d, %d" % (drive_mode[0], drive_mode[1]))
                 #print(id(drive_mode))
                 if dvm[0] != drive_mode[0] or dvm[1] != drive_mode[1]:
                     try:
@@ -181,7 +180,7 @@ class TeamOverload(object):
                         pass
                     dvm[0] = drive_mode[0]
                     dvm[1] = drive_mode[1]
-                    driver = Process(target=drive, args=(dvm[0], turngap, dvm[1]))
+                    driver = Process(target=drive, args=(dvm[0], turngap, read_z(), dvm[1]))
                     driver.start()
                     print("driver was started.")
 
@@ -191,7 +190,7 @@ class TeamOverload(object):
             while True:
                 front_r, bottom_r, _, bottom_l, _, front_l = get_data()
                 print((front_r, bottom_r, bottom_l, front_l))
-                print("linetracer: %d, %d" % (drive_mode[0], drive_mode[1]))
+                #print("linetracer: %d, %d" % (drive_mode[0], drive_mode[1]))
                 #print(id(drive_mode))
 
                 if frontsensor and (front_l > threshold or front_r > threshold) if not drive_mode[1] else \
