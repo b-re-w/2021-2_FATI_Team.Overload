9e6b368881ab071f04f18d60057cb7096d2378af
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-09-04 10:38:45 +0900

    제한된 색상 인식 함수 수정

--- My_Projects/Jupyter/FATI_Main.py
+++ My_Projects/Jupyter/FATI_Main.py
@@ -124,7 +124,7 @@ class TeamOverload(object):
         *  'hv' : real Blue -> Yellow, real Orange -> Blue, real Yellow -> Blue
         *  'h' : real Blue -> Yellow, real orange -> Blue, real Yellow -> Blue
            's' : real Blue -> Blue, real orange -> Blue, real Yellow -> Blue or Yellow or Orange
-           'v' : real Blue -> Yellow, real orange -> Blue or Yellow, real Yellow -> Yellow
+        *  'v' : real Blue -> Yellow, real orange -> Blue or Yellow, real Yellow -> Yellow
            so, use 'sv' to separate Yellow from the others, and use 'hv' to distinguish blue from yellow
            (if hv yellow then blue, and hv blue then orange)
         """
@@ -140,7 +140,11 @@ class TeamOverload(object):
                 if knn.predict(image) == "Yellow":
                     knn.fit("h")
                     if knn.predict(image) == "Blue":
-                        result = "Yellow"
+                        knn.fit("v")
+                        if knn.predict(image) == "Blue":
+                            result = "Orange"
+                        else:
+                            result = "Yellow"
                     else:  # prediction == "Yellow"
                         result = "Blue"
                 else:
